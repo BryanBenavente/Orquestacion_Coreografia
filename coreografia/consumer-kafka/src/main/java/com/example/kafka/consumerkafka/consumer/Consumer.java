bc2256ca7ab0e47396bb0ecefe587880319c151f
bbenaven-everis <bryan.paul.benavente.casas@nttdata.com>
2022-06-07 23:25:25 -0500

    Kafka consumer

--- coreografia/consumer-kafka/src/main/java/com/example/kafka/consumerkafka/consumer/Consumer.java
+++ coreografia/consumer-kafka/src/main/java/com/example/kafka/consumerkafka/consumer/Consumer.java
@@ -3,6 +3,9 @@ package com.example.kafka.consumerkafka.consumer;
 import com.example.kafka.consumerkafka.model.Product;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -10,8 +13,8 @@ import org.springframework.stereotype.Component;
 public class Consumer {
 
     @KafkaListener(topics = "topic-demo", containerFactory = "kafkaListenerContainerFactory")
-    public void listen(Product product) {
-        log.info("Mensaje recibido: " + product);
+    public void listen(@Payload Product msg, @Headers MessageHeaders headers) {
+        log.info("Mensaje recibido: " + msg);
     }
 
 }
